93ef84d202f91a132ae8d98673ce01c099ec769f
Keyuan Li <keyuanli@Keyuans-MBP.fios-router.home>
2019-09-24 20:44:26 -0400

    clean typo

--- chip8/cpu/util.go
+++ chip8/cpu/util.go
@@ -1,9 +1,9 @@
 package cpu
 
 func BCD(value byte) (a, b, c byte) {
-	a := data / 100
-	b := (data % 100) / 10
-	c := data % 10
+	a = value / 100
+	b = (value % 100) / 10
+	c = value % 10
 
 	return a, b, c
 }
