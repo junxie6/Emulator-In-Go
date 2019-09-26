d4ce01691f5394c3f33889d4471c57f3c6c22252
Keyuan Li <keyuanli@Keyuans-MBP.fios-router.home>
2019-09-25 22:35:55 -0400

    spilt register id

--- gameboy/cpu/const.go
+++ gameboy/cpu/const.go
@@ -2,11 +2,22 @@ package cpu
 
 type registerID int
 
+const HiLoMagic = 3
+
 const (
-	AF registerID = iota
+	A registerID = iota
+	F
+	AF
+	B
+	C
 	BC
+	D
+	E
 	DE
+	H
+	L
 	HL
+	Split
 	SP
 	PC
 )
