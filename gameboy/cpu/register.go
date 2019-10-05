9382601b99368b0c8c33909a06afc00b1727e7e6
Keyuan <keyuanli123@gmail.com>
2019-10-05 17:54:54 -0400

    split 8bit and 16bit register and add a pool to store them

--- gameboy/cpu/register.go
+++ gameboy/cpu/register.go
@@ -1,43 +1,103 @@
 package cpu
 
-//Register define 16bit register
-type Register uint16
+import "fmt"
 
-func preloadRegister() map[registerID]*Register {
-	return map[registerID]*Register{
-		AF: new(Register),
-		BC: new(Register),
-		DE: new(Register),
-		HL: new(Register),
-		SP: new(Register),
-		PC: new(Register),
+type Register interface {
+	Write(value uint16)
+	Read() uint16
+}
+
+type Register8bitWrap struct {
+	write func(value byte)
+	read  func() byte
+}
+
+func (r *Register8bitWrap) Write(value uint16) {
+	r.write(byte(value))
+}
+
+func (r *Register8bitWrap) Read() uint16 {
+	return uint16(r.read())
+}
+
+type RegisterPool struct {
+	list map[registerID]*Register16bit
+}
+
+func (p *RegisterPool) Get(id registerID) Register {
+	// var id registerID
+	var _id registerID
+	switch id {
+	case A, F, AF:
+		_id = AF
+	case B, C, BC:
+		_id = BC
+	case D, E, DE:
+		_id = DE
+	case H, L, HL:
+		_id = HL
+	default:
+		panic(fmt.Sprintf("unknow id %d", id))
+	}
+
+	target := p.list[id]
+
+	switch id {
+	case AF, BC, DE, HL, SP, PC:
+		return target
+	case A, B, D, H:
+		return &Register8bitWrap{
+			write: target.WriteHi,
+			read:  target.ReadLo,
+		}
+	case F, C, E, L:
+		return &Register8bitWrap{
+			write: target.WriteLo,
+			read:  target.ReadLo,
+		}
+	default:
+		panic(fmt.Sprintf("unknow id %d", id))
 	}
 }
 
-func (r *Register) Write(value uint16) {
-	*r = Register(value)
+//Register16bit define 16bit register
+type Register16bit uint16
+
+func preloadRegister() map[registerID]Register {
+	return map[registerID]Register{
+		AF: new(Register16bit),
+		BC: new(Register16bit),
+		DE: new(Register16bit),
+		HL: new(Register16bit),
+		SP: new(Register16bit),
+		PC: new(Register16bit),
+	}
 }
 
-func (r *Register) Read() uint16 {
+func (r *Register16bit) Write(value uint16) {
+	*r = Register16bit(value)
+}
+
+func (r *Register16bit) Read() uint16 {
 	return uint16(*r)
 }
 
 //WriteHi write data to hi
-func (r *Register) WriteHi(hi byte) {
-	*r = (Register(hi) << 8) | (*r & 0x00ff)
+func (r *Register16bit) WriteHi(hi byte) {
+	*r = (Register16bit(hi) << 8) | (*r & 0x00ff)
 }
 
 //ReadHi read data from hi
-func (r *Register) ReadHi() byte {
+func (r *Register16bit) ReadHi() byte {
 	return byte((*r & 0xff00) >> 8)
 }
 
 //WriteLo write data to lo
-func (r *Register) WriteLo(lo byte) {
-	*r = (*r & 0xff00) | Register(lo)
+func (r *Register16bit) WriteLo(lo byte) {
+	*r = (*r & 0xff00) | Register16bit(lo)
 }
 
 //ReadLo read data from lo
-func (r *Register) ReadLo() byte {
+func (r *Register16bit) ReadLo() byte {
 	return byte(*r & 0x00ff)
 }
