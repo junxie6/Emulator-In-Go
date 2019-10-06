a41467d41eb8c7e59fd236c67ca237dc9bba4885
Keyuan <keyuanli123@gmail.com>
2019-10-06 15:06:58 -0400

    add util func to check carry

--- gameboy/util/bit.go
+++ gameboy/util/bit.go
@@ -4,3 +4,23 @@ package util
 func ByteCombine(hi, lo byte) uint16 {
 	return (uint16(hi) << 8) | uint16(lo)
 }
+
+//HalfCarryForAdd check if half carry happened for a+b
+func HalfCarryForAdd(a, b uint16) bool {
+	return (a&0xf)+(b&0xf) > 0xf
+}
+
+//HalfCarryForSub check if half carry happened for a-b
+func HalfCarryForSub(a, b uint16) bool {
+	return (a & 0xf) < (b & 0xf)
+}
+
+//CarryForAdd check if carry happened for a+b
+func CarryForAdd(a, b uint16) bool {
+	return (uint16(a)&0xf)+(uint16(b)&0xf) > 0xf
+}
+
+//CarryForSub check if carry happend for a-b
+func CarryForSub(a, b uint16) bool {
+	return (a & 0xff) < (b & 0xff)
+}
