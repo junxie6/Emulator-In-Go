edbb2b61ea799cf2de07a37de44e09016bb77d72
Keyuan <keyuanli123@gmail.com>
2019-10-06 15:38:34 -0400

    change func name

--- gameboy/util/bit.go
+++ gameboy/util/bit.go
@@ -5,22 +5,22 @@ func ByteCombine(hi, lo byte) uint16 {
 	return (uint16(hi) << 8) | uint16(lo)
 }
 
-//HalfCarryForAdd check if half carry happened for a+b
-func HalfCarryForAdd(a, b uint16) bool {
+//HalfCarry check if half carry happened for a+b
+func HalfCarry(a, b uint16) bool {
 	return (a&0xf)+(b&0xf) > 0xf
 }
 
-//HalfCarryForSub check if half carry happened for a-b
-func HalfCarryForSub(a, b uint16) bool {
+//HalfBorrow check if half carry happened for a-b
+func HalfBorrow(a, b uint16) bool {
 	return (a & 0xf) < (b & 0xf)
 }
 
-//CarryForAdd check if carry happened for a+b
-func CarryForAdd(a, b uint16) bool {
+//Carry check if carry happened for a+b
+func Carry(a, b uint16) bool {
 	return (uint16(a)&0xf)+(uint16(b)&0xf) > 0xf
 }
 
-//CarryForSub check if carry happend for a-b
-func CarryForSub(a, b uint16) bool {
+//Borrow check if carry happend for a-b
+func Borrow(a, b uint16) bool {
 	return (a & 0xff) < (b & 0xff)
 }
